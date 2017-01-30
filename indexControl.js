9fbd1c63728d0226d2e638cd43bd8de25739641d
scott henderson <hendscot@gmail.com>
2017-01-30 16:53:19 -0500

    added sidebar text

--- indexControl.js
+++ indexControl.js
@@ -1,6 +1,14 @@
+let anchors = ['Projects', 'About', 'Contact', 'GitHub', 'RSS'];
+let navAnchors = document.getElementsByClassName('text');
 document.getElementById('cmt-nav').onmouseenter = function (e) {
     document.getElementById('cmt-bdy-ovrly').className = 'dim';
+    for (var i = 0; i < navAnchors.length; i++) {
+        navAnchors[i].innerHTML = anchors[i];
+    }
 }
 document.getElementById('cmt-nav').onmouseleave = function (e) {
     document.getElementById('cmt-bdy-ovrly').className = 'transparent';
+    for (var i = 0; i < navAnchors.length; i++) {
+        navAnchors[i].innerHTML = null;
+    }
 }
